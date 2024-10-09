4684c4151ba2ebdeef0814a342015a77deb966dd
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-10-09 18:02:34 +0900

    #1 [add] snackBar 확장함수

--- app/src/main/java/org/sopt/and/core/Context.kt
+++ app/src/main/java/org/sopt/and/core/Context.kt
@@ -2,6 +2,10 @@ package org.sopt.and.core
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -9,4 +13,19 @@ fun Context.showToast(message: String) {
 
 fun Context.showLongToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+
+@Composable
+fun Context.showSnackar(snackbarHostState: SnackbarHostState, message: String) {
+    LaunchedEffect(message) {
+        snackbarHostState.showSnackbar(message)
+    }
+}
+
+@Composable
+fun Context.showLongSnackbar(snackbarHostState: SnackbarHostState, message: String) {
+    LaunchedEffect(message) {
+        snackbarHostState.showSnackbar(message, duration = SnackbarDuration.Long)
+    }
 }
