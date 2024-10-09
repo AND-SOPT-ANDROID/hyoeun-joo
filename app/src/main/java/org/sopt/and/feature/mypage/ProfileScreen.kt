5fb2719164c1b9406ba6d7035a4aa626a7c880f4
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-10-09 17:32:43 +0900

    #1 [mod] ui 변경

--- app/src/main/java/org/sopt/and/feature/mypage/ProfileScreen.kt
+++ app/src/main/java/org/sopt/and/feature/mypage/ProfileScreen.kt
@@ -17,6 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.sopt.and.R
@@ -43,7 +46,7 @@ fun ProfileScreen(userName: String) {
                 alignment = Alignment.CenterStart
             )
             Text(
-                userName + "님",
+                userName + stringResource(R.string.person),
                 color = Color.White,
                 modifier = Modifier
                     .weight(1f)
@@ -57,11 +60,91 @@ fun ProfileScreen(userName: String) {
                 alignment = Alignment.CenterEnd
             )
         }
-        MyPagePurchase("첫 결제 시 첫 달 100원!")
-        Spacer(modifier = Modifier.padding(top = 8.dp))
-        MyPagePurchase("현재 보유하신 이용권이 없습니다. ")
-        VideoList("전체 시청내역", "시청내역이 없어요.")
-        VideoList("관심 프로그램", "관심 프로그램이 없어요.")
+        MyPagePurchase(stringResource(R.string.profile_first_purchase_description))
+        Spacer(modifier = Modifier.padding(top = 4.dp))
+        MyPagePurchase(stringResource(R.string.profile_no_ticket))
+        VideoList(
+            stringResource(R.string.profile_total_view_history),
+            stringResource(R.string.profile_no_view_history)
+        )
+        VideoList(
+            stringResource(R.string.profile_interest_program),
+            stringResource(R.string.profile_no_interest_program)
+        )
 
+    }
+}
+
+@Composable
+fun MyPagePurchase(description: String) {
+    Column(
+        modifier = Modifier
+            .background(color = Color(0xFF252525))
+            .padding(top = 16.dp)
+            .padding(horizontal = 10.dp)
+    ) {
+
+        Text(description, color = Color(0xFFA5A5A5))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Text("구매하기", color = Color.White, fontSize = 20.sp)
+            Image(
+                painter = painterResource(R.drawable.ic_back_right_white_24),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
+    }
+}
+
+@Composable
+fun VideoList(videoDescription: String, emptyDescription: String? = null) {
+    Column(
+        modifier = Modifier
+            .background(color = Color(0xFF1B1B1B))
+            .padding(horizontal = 10.dp)
+            .padding(bottom = 20.dp)
+    ) {
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Text(
+            videoDescription,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            color = Color.White,
+            fontSize = 16.sp
+        )
+        EmptyVideoList(emptyDescription)
+    }
+}
+
+//나중에 EmptyView로 갈아끼우기 위해 함수분리
+@Composable
+fun EmptyVideoList(emptyDescription: String?) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+    ) {
+        Spacer(modifier = Modifier.padding(top = 40.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_error_gray_24),
+            contentDescription = "Empty Video List",
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(60.dp),
+            alignment = Alignment.Center
+        )
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        emptyDescription?.let {
+            Text(
+                it, color = Color(0xFFA6A6A6),
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally),
+
+                )
+        }
     }
 }
