09db203b5b2390d832be27dc32768e363262e206
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-10-09 13:59:30 +0900

    #1 [feat] Divider

---
+++ app/src/main/java/org/sopt/and/core/Context.kt
@@ -0,0 +1,12 @@
+package org.sopt.and.core
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showLongToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
