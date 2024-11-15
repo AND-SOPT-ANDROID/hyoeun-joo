fa60cf2261d3aa0caf203e0ada327b8d25c921be
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-11-15 22:36:19 +0900

    #7 [feat] MyPageScreen 서버 통신 로직 구현

--- app/src/main/java/org/sopt/and/feature/mypage/MyPageScreen.kt
+++ app/src/main/java/org/sopt/and/feature/mypage/MyPageScreen.kt
@@ -8,20 +8,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import org.sopt.and.feature.model.UserInfo
 import org.sopt.and.feature.nav.BottomNavGraph
 import org.sopt.and.feature.nav.BottomNavigation
-import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 
 @Composable
-fun MyPageScreen(userInfo: UserInfo, viewModel: MyPageViewModel) {
-    viewModel.setEmail(userInfo)
-
+fun MyPageScreen() {
     val navController = rememberNavController()
-
     Scaffold(
         bottomBar = { BottomNavigation(navController = navController) }
     ) {
@@ -33,15 +27,7 @@ fun MyPageScreen(userInfo: UserInfo, viewModel: MyPageViewModel) {
         ) {
             BottomNavGraph(
                 navController = navController,
-                viewModel = viewModel
             )
         }
-    }
-}
-
-@Preview
-@Composable
-fun GreetingPreview2() {
-    ANDANDROIDTheme {
     }
 }
--- app/src/main/java/org/sopt/and/feature/mypage/MyPageViewModel.kt
+++ app/src/main/java/org/sopt/and/feature/mypage/MyPageViewModel.kt
@@ -1,16 +1,31 @@
 package org.sopt.and.feature.mypage
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import org.sopt.and.feature.model.UserInfo
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.sopt.and.domain.repository.MyPageRepository
+import javax.inject.Inject
 
-class MyPageViewModel : ViewModel() {
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val myPageRepository: MyPageRepository
+) : ViewModel() {
 
-    private val _email = MutableStateFlow("")
-    val email: StateFlow<String> = _email
+    private val _hobby = MutableStateFlow<String?>(null)
+    val hobby: StateFlow<String?> = _hobby.asStateFlow()
 
-    fun setEmail(userInfo: UserInfo) {
-        _email.value = userInfo.id
+    fun loadHobby(token: String) {
+        viewModelScope.launch {
+            val result = myPageRepository.getMyHobby(token)
+            result.onSuccess { response ->
+                _hobby.value = response.hobby
+            }.onFailure {
+                _hobby.value = "데이터를 불러오는데 실패했습니다."
+            }
+        }
     }
 }
