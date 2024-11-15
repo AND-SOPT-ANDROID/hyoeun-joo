9fc74dffd75d745ed8586ded300f6343ae46bf32
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-11-15 22:38:19 +0900

    #7 [feat] Error 함수들 구현

---
+++ app/src/main/java/org/sopt/and/domain/utils/Error.kt
@@ -0,0 +1,19 @@
+package org.sopt.and.domain.utils
+
+sealed class Error(message: String?) : Exception(message) {
+    data class ApiError(
+        val errorMessage: String?,
+    ) : Error(errorMessage)
+
+    data class NetWorkConnectError(
+        val errorMessage: String,
+    ) : Error(errorMessage)
+
+    data class TimeOutError(
+        val errorMessage: String,
+    ) : Error(errorMessage)
+
+    data class UnknownError(
+        val errorMessage: String,
+    ) : Error(errorMessage)
+}
